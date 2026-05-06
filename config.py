e1f6d51fe1c115ad1ea0e720cc45e99224c0c61c
mafiozydev <mafiozydev@gmail.com>
2026-05-06 13:05:49 +0400

    Update config.py with MAX_PRICE_USD and Soulway focus

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-# Конфигурация парсера Ozon
+import os
+from dotenv import load_dotenv
 
-MAX_PRICE_RUB = 5000  # ~50 USD, меняй по необходимости
+load_dotenv()
 
-BRANDS_TO_TRACK = [
-    "Soulway",
+MAX_PRICE_USD = float(os.getenv("MAX_PRICE_USD", 50))
+MAX_PRICE_RUB = MAX_PRICE_USD * 80  # примерный курс
+
+BRAND_URLS = [
+    "https://www.ozon.ru/brand/soul-way-100918703/",
 ]
 
 SEARCH_QUERIES = [
     "soulway",
-    "soul way",
+    "soulway протеин",
+    "soulway bcaa",
 ]
-
-MAX_PAGES_PER_QUERY = 3
-DELAY_BETWEEN_ACTIONS = 2.5
-
-DB_PATH = "prices.db"
