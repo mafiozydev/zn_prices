870692b4229cc3cde295b205f347235043af5721
mafiozydev <mafiozydev@gmail.com>
2026-05-06 12:42:04 +0400

    Add config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# Конфигурация парсера Ozon
+
+MAX_PRICE_RUB = 5000  # ~50 USD, меняй по необходимости
+
+BRANDS_TO_TRACK = [
+    "Soulway",
+]
+
+SEARCH_QUERIES = [
+    "soulway",
+    "soul way",
+]
+
+MAX_PAGES_PER_QUERY = 3
+DELAY_BETWEEN_ACTIONS = 2.5
+
+DB_PATH = "prices.db"
